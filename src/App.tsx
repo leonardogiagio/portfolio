73279310207ff203826d0861fabe63100ed4125b
leonardogiagio <leonardogiagio@gmail.com>
2023-06-05 03:32:48 -0300

    first commit portfolio

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { About } from "./components/About";
+import { Hero } from "./components/Hero";
+import { Services } from "./components/Services";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
+import { Testimonials } from "./components/Testimonials";
+import { Contact } from "./components/Contact";
+import "./App.css";
+
+function App() {
+	return (
+		<main>
+			<Hero />
+			<About />
+			<Services />
+			<Projects />
+			<Skills />
+			<Testimonials />
+			<Contact />
+		</main>
+	);
+}
+
+export default App;
